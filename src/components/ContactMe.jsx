22303197e0b7f18a38e31dc46974234582357999
JavierMartorano <martorano.javi@gmail.com>
2023-01-19 20:27:22 -0300

    Contacto Funcionando

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,29 +1,35 @@
-import * as React from "react";
-import {
-  Container,
-  Form,
-  Button,
-  Row,
-  Col
-} from "react-bootstrap";
-
+import React, { useEffect, useState } from "react";
+import { Container, Form, Button, Row, Col } from "react-bootstrap";
+import axios from "axios";
 import { useForm } from "react-hook-form";
 import "../assets/css/contactMe.css";
 
 const ContactMe = () => {
   const initState = {
+    nombre: "",
+    apellido: "",
     email: "",
-    password: "",
-    confirmPassword: "",
-    termsAndConditions: false
+    telefono: "",
+    consulta: "",
   };
 
-  // eslint-disable-next-line no-unused-vars
-  const [initialValues, setInitialValues] = React.useState(initState);
+  const [isSending, setSending] = useState(false);
+  const [initialValues, setInitialValues] = useState(initState);
 
   const onSubmit = (values) => {
-    console.log("Values:::", values);
-    console.log("Values:::", JSON.stringify(values));
+    axios({
+      method: "POST",
+      url: "https://formbold.com/s/9xnGX",
+      data: values,
+    })
+      .then((r) => {
+        setSending(true)
+        console.log("Recibido");
+      })
+      .catch((r) => {
+        console.log("error");
+      });
+    // console.log("Values:::", JSON.stringify(values));
   };
 
   const onError = (error) => {
@@ -35,108 +41,113 @@ const ContactMe = () => {
     handleSubmit,
     getValues,
     watch,
-    formState: { errors }
+    formState: { errors },
   } = useForm({
     mode: "onTouched",
     reValidateMode: "onSubmit",
-    // reValidateMode: "onChange",
-    defaultValues: initialValues
+    defaultValues: initialValues,
   });
 
-  // const x = JSON.stringify(data);
-  // const y = JSON.stringify(listShow);
+  // useEffect(() => {
+  //   const subscription = watch((value, { name, type }) => {
+  //     // console.log(">>", value, name, type);
+  //     // {1: '1', 2: '9'} '2' 'change'
+  //   });
 
-  React.useEffect(() => {
-    const subscription = watch((value, { name, type }) => {
-      console.log(">>", value, name, type);
-      // {1: '1', 2: '9'} '2' 'change'
-    });
+  //   return () => subscription.unsubscribe();
+  // }, [watch]);
 
-    return () => subscription.unsubscribe();
-  }, [watch]);
-    
   return (
     <div className="containerContactMe">
       <h2>Contacto</h2>
       <Container className="my-4 form">
-      <Form onSubmit={handleSubmit(onSubmit, onError)}>
+        <Form onSubmit={handleSubmit(onSubmit, onError)}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Nombre</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Ingrese su nombre"
+              {...register("nombre", { required: "El nombre es obligatorio" })}
+            />
+            {errors.nombre && (
+              <Form.Text className="text-danger">
+                {errors.nombre.message}
+              </Form.Text>
+            )}
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Nombre</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Ingrese su nombre"
-            {...register("email", { required: "El nombre es obligatorio" })}
-          />
-          {errors.email && (
-            <Form.Text className="text-danger">
-              {errors.email.message}
-            </Form.Text>
-          )}
-        </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Apellido</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Ingrese su apellido"
+              {...register("apellido", {
+                required: "El apellido es obligatorio",
+              })}
+            />
+            {errors.apellido && (
+              <Form.Text className="text-danger">
+                {errors.apellido.message}
+              </Form.Text>
+            )}
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Apellido</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Ingrese su apellido"
-            {...register("email", { required: "El apellido es obligatorio" })}
-          />
-          {errors.email && (
-            <Form.Text className="text-danger">
-              {errors.email.message}
-            </Form.Text>
-          )}
-        </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Ingresar correo"
+              {...register("email", { required: "El correo es obligatorio" })}
+            />
+            {errors.email && (
+              <Form.Text className="text-danger">
+                {errors.email.message}
+              </Form.Text>
+            )}
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Ingresar correo"
-            {...register("email", { required: "El correo es obligatorio" })}
-          />
-          {errors.email && (
-            <Form.Text className="text-danger">
-              {errors.email.message}
-            </Form.Text>
-          )}
-        </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Teléfono</Form.Label>
+            <Form.Control
+              type="tel"
+              placeholder="Ingresar su teléfono"
+              {...register("telefono", {
+                required: "El numero de teléfono es obligatorio",
+              })}
+            />
+            {errors.telefono && (
+              <Form.Text className="text-danger">
+                {errors.telefono.message}
+              </Form.Text>
+            )}
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Teléfono</Form.Label>
-          <Form.Control
-            type="tel"
-            placeholder="Ingresar su teléfono"
-            {...register("email", { required: "El numero de teléfono es obligatorio" })}
-          />
-          {errors.email && (
-            <Form.Text className="text-danger">
-              {errors.email.message}
-            </Form.Text>
-          )}
-        </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Consulta</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={3}
+              placeholder="Ingrese su consulta"
+              {...register("consulta", {
+                required: "La consulta es obligatoria",
+              })}
+            />
+            {errors.consulta && (
+              <Form.Text className="text-danger">
+                {errors.consulta.message}
+              </Form.Text>
+            )}
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Consulta</Form.Label>
-          <Form.Control
-            as="textarea" rows={3}
-            placeholder="Ingrese su consulta"
-            {...register("email", { required: "La consulta es obligatoria" })}
-          />
-          {errors.email && (
-            <Form.Text className="text-danger">
-              {errors.email.message}
-            </Form.Text>
-          )}
-        </Form.Group>
-        
-        <Button variant="primary" type="submit">
-          Enviar
-        </Button>
-      </Form>
-    </Container>
-
+          <Button
+            variant="secondary"
+            type="submit"
+            disabled={isSending}
+          >
+            {isSending ? "Recibido" : "Enviar"}
+          </Button>
+        </Form>
+      </Container>
     </div>
   );
 };
