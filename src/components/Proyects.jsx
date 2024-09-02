fff7325cfd463fb7c111ec4f3e5d8f578c585c4d
JavierMartorano <martorano.javi@gmail.com>
2024-09-02 18:04:52 -0300

    changes

--- src/components/Proyects.jsx
+++ src/components/Proyects.jsx
@@ -14,14 +14,16 @@ const projects = [
     src: SomosCorpora,
     alt: "SomosCorpora Project Video",
     link: "https://somoscorpora.com/",
-    description: "Migración del sitio web de SomosCorpora de PHP a un stack moderno utilizando React y Tailwind para el frontend y Python Flask para el backend. Se mejoró significativamente la experiencia del usuario mediante nuevas animaciones y estilos actualizados."
+    description:
+      "Migración del sitio web de SomosCorpora de PHP a un stack moderno utilizando React y Tailwind para el frontend y Python Flask para el backend. Se mejoró significativamente la experiencia del usuario mediante nuevas animaciones y estilos actualizados.",
   },
   {
     name: "Akuario",
     mediaType: "video",
     src: Akuario,
     alt: "Akuario Project Video",
-    description: "Desarrollo completo de un ecommerce innovador junto a otro desarrollador. El proyecto incluyó un diseño único y funcionalidades avanzadas, como la visualización de estadísticas de usuario y un scrapper para mostrar la variación de distintos tipos de moneda en tiempo real. Tecnologías utilizadas: React, Python Flask, MySQL."
+    description:
+      "Desarrollo completo de un ecommerce innovador junto a otro desarrollador. El proyecto incluyó un diseño único y funcionalidades avanzadas, como la visualización de estadísticas de usuario y un scrapper para mostrar la variación de distintos tipos de moneda en tiempo real. Tecnologías utilizadas: React, Python Flask, MySQL.",
   },
   {
     name: "Dosados",
@@ -29,28 +31,32 @@ const projects = [
     src: Dosados,
     alt: "Dosados Project Video",
     link: "https://dosados.com.ar/",
-    description: "Realización de modificaciones y mantenimiento de la landing page de Dosados, una empresa de desarrollos inmobiliarios. Trabajé con una aplicación existente hecha en HTML, CSS y JavaScript, mejorando la interfaz de usuario y ofreciendo un servicio de mantenimiento continuo."
+    description:
+      "Realización de modificaciones y mantenimiento de la landing page de Dosados, una empresa de desarrollos inmobiliarios. Trabajé con una aplicación existente hecha en HTML, CSS y JavaScript, mejorando la interfaz de usuario y ofreciendo un servicio de mantenimiento continuo.",
   },
   {
     name: "Matchmaker",
     mediaType: "image",
     src: MatchMaker,
     alt: "Matchmaker Project Image",
-    description: "Aplicación de React Native para organizar reuniones o partidos de forma sencilla con amigos, con perfiles editables y la opción de crear torneos."
+    description:
+      "Aplicación de React Native para organizar reuniones o partidos de forma sencilla con amigos, con perfiles editables y la opción de crear torneos.",
   },
   {
     name: "Gower",
     mediaType: "video",
     src: CarCalendar,
     alt: "Calendario Project Video",
-    description: "Desarrollo de una plataforma web en React para la gestión de reservas de vehículos de alquiler. Incluye una interfaz de calendario intuitiva y un avanzado sistema de filtros, integrado con una API que utiliza una hoja de cálculo como base de datos."
+    description:
+      "Desarrollo de una plataforma web en React para la gestión de reservas de vehículos de alquiler. Incluye una interfaz de calendario intuitiva y un avanzado sistema de filtros, integrado con una API que utiliza una hoja de cálculo como base de datos.",
   },
   {
     name: "Constructora Colonial",
     mediaType: "video",
     src: ConstructoraColonial,
     alt: "Constructora Colonial Project Video",
-    description: "Desarrollo en conjunto con Corpora para la empresa Constructora Colonial, migrando su sistema de trabajo interno de PHP a un stack moderno con React, Material UI, Python Flask, y MySQL, mejorando la eficiencia operativa y la experiencia del usuario interno."
+    description:
+      "Desarrollo en conjunto con Corpora para la empresa Constructora Colonial, migrando su sistema de trabajo interno de PHP a un stack moderno con React, Material UI, Python Flask, y MySQL, mejorando la eficiencia operativa y la experiencia del usuario interno.",
   },
 ];
 
@@ -92,12 +98,12 @@ const Proyects = () => {
                     href={project.link}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="font-poppins text-lg font-semibold text-white underline"
+                    className="font-poppins text-lg font-semibold text-teal-400 cursor-pointer"
                   >
                     {project.name}
                   </a>
                 ) : (
-                  <h3 className="font-poppins text-lg font-semibold text-white">
+                  <h3 className="font-poppins text-lg font-semibold text-teal-400 cursor-pointer">
                     {project.name}
                   </h3>
                 )}
@@ -109,13 +115,14 @@ const Proyects = () => {
         {selectedProject && (
           <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50 transition-opacity duration-300 ease-in-out">
             <div className="relative bg-gray-900 rounded-lg overflow-hidden max-w-2xl mx-auto transform transition-transform duration-300 ease-in-out scale-105">
-              {/* Botón de cierre colocado aquí para que esté flotando sobre el fondo */}
               <button
-                className="absolute top-4 right-4 text-white text-4xl cursor-pointer z-50 font-bold"
+                className="absolute top-4 right-4 bg-black bg-opacity-50 text-white border border-white rounded-md text-xl cursor-pointer flex items-center justify-center p-1 z-50"
                 onClick={() => setSelectedProject(null)}
+                style={{ width: "30px", height: "30px", pointerEvents: "auto" }} // Asegura que el botón reciba eventos de puntero
               >
                 ✕
               </button>
+
               {selectedProject.mediaType === "image" ? (
                 <img
                   className="w-full h-auto"
@@ -133,16 +140,18 @@ const Proyects = () => {
               )}
               <div className="p-4 text-center">
                 {selectedProject.link ? (
-                  <a
-                    href={selectedProject.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="font-poppins text-2xl font-semibold text-teal-400 mb-4 underline"
-                  >
-                    {selectedProject.name}
-                  </a>
+                  <div className="mb-4">
+                    <a
+                      href={selectedProject.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="font-poppins text-2xl font-semibold text-teal-400 cursor-pointer"
+                    >
+                      {selectedProject.name}
+                    </a>
+                  </div>
                 ) : (
-                  <h3 className="font-poppins text-2xl font-semibold text-white mb-4">
+                  <h3 className="font-poppins text-2xl font-semibold text-teal-400 mb-4 cursor-pointer">
                     {selectedProject.name}
                   </h3>
                 )}
